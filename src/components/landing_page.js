4370364ba061f8f8ce26a7829cb9d30b13636600
Nikko Ortega <NFCOrtega89@gmail.com>
2017-04-20 22:08:25 -0700

    travis

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -5,7 +5,7 @@ export default class LandingPage extends Component {
   render () {
     return (
       <div className="jumbotron">
-        <h1>AirTime</h1>
+        <h1>Time to fly drones!</h1>
         <p>This MVP application provides drone users with a lists of flyzones along with current weather conditions.</p>
         <h3>Select City</h3>
         <ul>
